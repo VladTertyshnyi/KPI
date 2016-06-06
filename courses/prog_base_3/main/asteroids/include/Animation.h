c27e18288324224582d02f3d86df1488192bf666
VladTertyshnyi <fpm.kp.51@gmail.com>
2016-06-07 00:57:06 +0300

    Refactoring and OOP
    Animation class now have it's own file.

---
+++ courses/prog_base_3/main/asteroids/include/Animation.h
@@ -0,0 +1,26 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+#include <SFML/Graphics.hpp>
+#include <list>
+using namespace sf;
+
+class Animation
+{
+    public:
+        Animation();
+        float Frame, speed;
+        Sprite sprite;
+        std::vector<IntRect> frames;
+
+        Animation (Texture &t, int x, int y, int w, int h, int count, float Speed);
+        void update();
+        bool isEnd();
+
+        virtual ~Animation();
+
+    protected:
+
+    private:
+};
+
+#endif // ANIMATION_H
--- courses/prog_base_3/main/asteroids/main.cpp
+++ courses/prog_base_3/main/asteroids/main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
 #include <list>
+#include "Animation.h"
+
 using namespace sf;
 
 const int W = 1200;
@@ -8,43 +10,7 @@ const int H = 700;
 
 float DEGTORAD = 0.017453f;
 
-class Animation
-{
-public:
-	float Frame, speed;
-	Sprite sprite;
-    std::vector<IntRect> frames;
 
-	Animation(){}
-
-    Animation (Texture &t, int x, int y, int w, int h, int count, float Speed)
-	{
-	    Frame = 0;
-        speed = Speed;
-
-		for (int i=0;i<count;i++)
-         frames.push_back( IntRect(x+i*w, y, w, h)  );
-
-		sprite.setTexture(t);
-		sprite.setOrigin(w/2,h/2);
-        sprite.setTextureRect(frames[0]);
-	}
-
-
-	void update()
-	{
-    	Frame += speed;
-		int n = frames.size();
-		if (Frame >= n) Frame -= n;
-		if (n>0) sprite.setTextureRect( frames[int(Frame)] );
-	}
-
-	bool isEnd()
-	{
-	  return Frame+speed>=frames.size();
-	}
-
-};
 
 
 class Entity
